235ccac6c3629073b3903d2a4f8d0a6095dce72a
Eric Prud'hommeaux <eric@w3.org>
2012-03-24 11:45:14 -0400

    + nested transactions

--- pg.cpp
+++ pg.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 #include <postgresql/libpq-fe.h>
+
+#define LINEN std::cerr << __FILE__ "(" << __LINE__ << "): warning "
 
 struct SQLclient_Postgres {
     // see http://www.postgresql.org/docs/8.0/static/libpq-example.html
@@ -17,9 +20,44 @@ struct SQLclient_Postgres {
 	virtual ~PGResultException () throw() {  }
     };
 
-    PGconn *conn;
+    struct Transaction {
+	SQLclient_Postgres& client;
+	int depth;
+	bool committed;
+	Transaction (SQLclient_Postgres& client)
+	    : client(client), depth(client.transactionDepth), committed(false)
+	{
+	}
+	~Transaction () {
+	    if (client.transactionError)
+		client._endTransaction(false);
+	    else {
+		if (client.transactionDepth != depth)
+		    throw std::runtime_error
+			("closing transactions out of order");
+		client._endTransaction(committed);
+	    }
+	}
+	void commit () {
+	    if (committed)
+		throw std::runtime_error("transaction already committed");
+	    committed = true;
+	}
+    };
+
+    PGconn* conn;
+    int transactionDepth;
+    bool transactionError;
+
+    PGresult* Exec (PGconn* conn, const char* cmd) {
+	std::cerr << "-- " << cmd << "\n";
+	return PQexec(conn, cmd);
+    }
+    
+
     SQLclient_Postgres (std::string connStr)
-	: conn(PQconnectdb(connStr.c_str()))
+	: conn(PQconnectdb(connStr.c_str())),
+	  transactionDepth(0), transactionError(false)
     {
 	if (PQstatus(conn) != CONNECTION_OK)
 	    throw std::runtime_error(std::string("Connection failed: ")
@@ -29,24 +67,53 @@ struct SQLclient_Postgres {
 	PQfinish(conn);
     }
 
-    struct Transaction {
-	SQLclient_Postgres& client;
-	Transaction (SQLclient_Postgres& client)
-	    : client(client)
-	{
+    Transaction getTransaction () {
+	if (transactionDepth == 0) {
 	    // Start a transaction block.
-	    PGresult* res = PQexec(client.conn, "BEGIN");
+	    PGresult* res = Exec(conn, "BEGIN");
 	    if (PQresultStatus(res) != PGRES_COMMAND_OK)
 		throw PGResultException("BEGIN command failed: ", res);
 	    PQclear(res);
 	}
-	~Transaction () {
-	    PGresult* res = PQexec(client.conn, "END");
-	    PQclear(res);
-	}
-    };
 
-    void execute (std::string cmd, e_TransactionMode transactionMode = TRANS_None) {
+	std::stringstream cmd;
+	cmd << "SAVEPOINT s_" << transactionDepth+1;
+	PGresult* res = Exec(conn, cmd.str().c_str());
+	if (PQresultStatus(res) != PGRES_COMMAND_OK)
+	    throw PGResultException("SAVEPOINT failed: ", res);
+	PQclear(res);
+
+	++transactionDepth;
+	return Transaction(*this);
+    }
+
+    void _endTransaction (bool commit) {
+	std::stringstream cmd;
+	cmd << (commit ? "RELEASE" : "ROLLBACK TO")
+	    << " SAVEPOINT s_" << transactionDepth;
+	// LINEN << "cmd: " << cmd.str().c_str() << "\n";
+
+	--transactionDepth;
+
+	PGresult* res = Exec(conn, cmd.str().c_str());
+	// LINEN << "post Exec\n";
+	if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+	    transactionError = true; // error state for reaping Transactions
+	    throw PGResultException("ending SAVEPOINT failed: ", res);
+	}
+	// LINEN << "post check\n";
+	PQclear(res);
+	// LINEN << "post clear\n";
+
+	if (transactionDepth == 0) {
+	    res = Exec(conn, transactionError ? "ROLLBACK" : "COMMIT");
+	    PQclear(res);
+	    transactionError = false;
+	}
+    }
+
+    void execute (std::string cmd,
+		  e_TransactionMode transactionMode = TRANS_None) {
 
 	PGresult *res;
 	// Fetch rows, either via cursor or directly.
@@ -54,20 +121,20 @@ struct SQLclient_Postgres {
 
 	    // Prepend cmd with a cursor named "theCursor".
 	    cmd.insert(0, "DECLARE theCursor CURSOR FOR ");
-	    res = PQexec(conn, cmd.c_str());
+	    res = Exec(conn, cmd.c_str());
 	    if (PQresultStatus(res) != PGRES_COMMAND_OK)
 		throw PGResultException("BDECLARE CURSOR failed: ", res);
 	    PQclear(res);
 
 	    // Get all the data from the cursor.
-	    res = PQexec(conn, "FETCH ALL IN theCursor");
+	    res = Exec(conn, "FETCH ALL IN theCursor");
 	    if (PQresultStatus(res) != PGRES_TUPLES_OK)
 		throw PGResultException("FETCH ALL failed: ", res);
 	}
 	else {
 
 	    // Simply execute the command.
-	    res = PQexec(conn, cmd.c_str());
+	    res = Exec(conn, cmd.c_str());
 	    if (PQresultStatus(res) != PGRES_TUPLES_OK)
 		throw PGResultException("EXEC failed: ", res);
 	}
@@ -89,7 +156,7 @@ struct SQLclient_Postgres {
 
 	/* Close the cursor. */
 	if (transactionMode == TRANS_Cursor) {
-	    res = PQexec(conn, "CLOSE theCursor");
+	    res = Exec(conn, "CLOSE theCursor");
 	    PQclear(res);
 	}
     }
@@ -99,18 +166,23 @@ int main(int argc, char *argv[]) {
 	try {
 	    SQLclient_Postgres pg("dbname = eric");
 	    /* List the system catalog of databases (3x). */
-	    pg.execute("select * from pg_database");
+	    pg.execute("SELECT * FROM pg_database");
 	    {
-		SQLclient_Postgres::Transaction trans(pg);
-		pg.execute("select * from pg_database", SQLclient_Postgres::TRANS_Transaction);
-		pg.execute("select * from pg_database", SQLclient_Postgres::TRANS_Cursor);
+		SQLclient_Postgres::Transaction trans = pg.getTransaction();
+		pg.execute("SELECT * FROM pg_database");
+		{
+		    SQLclient_Postgres::Transaction tr2 = pg.getTransaction();
+		    pg.execute("SELECT * FROM pg_database",
+			       SQLclient_Postgres::TRANS_Cursor);
+		}
+		trans.commit();
 	    }
 	    // pg.execute("create table foo (id int);");
 	    // pg.execute("insert into foo (id) values (2);");
 	    // pg.execute("insert into foo (id) values (1);");
 	    // pg.execute("select * from foo;");
 	    // pg.execute("drop table foo;");
-	} catch (std::exception& e) {
+	} catch (std::runtime_error& e) {
 	    std::cerr << e.what();
 	    return 222;
 	}
